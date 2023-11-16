f16f333d7b25b19b0321c368e4150afba81bd387
Gokul-Srinivasan <150216211+Gokul-Srinivasan@users.noreply.github.com>
2023-11-16 12:22:02 +0530

    Add files via upload

---
+++ day2/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Styling from './Component/styling';
+function App() {
+  return (
+    <div className="App">
+    <Styling />
+    </div>
+  );
+}
+export default App;
---
+++ day2/src/Component/styling.js
@@ -0,0 +1,12 @@
+const Styling=()=>
+{
+    return(
+        <div>
+            <h1 style={{color:"green"}}>
+                Inline style in JSX Example
+            </h1>
+                <p style={{color:"darkblue",backgroundColor:"lightblue",fontSize:'16px'}}>This is a paragraph with inline styling</p>
+        </div>
+    );
+}
+export default Styling;
